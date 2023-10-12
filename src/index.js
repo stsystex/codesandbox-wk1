9aa1d30319fd01fafe69f94509cc587a668d2a80
s.t <147453475+stsystex@users.noreply.github.com>
2023-10-12 04:11:13 +0000

    Update 4 files

---
+++ src/index.js
@@ -0,0 +1,63 @@
+var varTest = "変数";
+console.log(varTest);
+
+let letTest = "定数";
+console.log(letTest);
+
+//letTest = "定数上書き";
+//console.log(letTest);
+
+//let letTest = "定数再宣言";
+//console.log(letTest);
+
+const constTest = "const変数";
+console.log(constTest);
+
+//const constTest = "const変数再宣言";
+
+const constAry = {
+  name: "test",
+  age: 28,
+};
+constAry.name = "test変更";
+console.log(constAry);
+
+const constList = ["list1", "list2"];
+constList[0] = "testchange";
+constList.push("testpush");
+console.log(constList);
+
+const name = "SYSTEX 太郎";
+const age = 28;
+
+const massage = "1私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(massage);
+
+const massage2 = "2私の名前は${name}です。年齢は${age}です。";
+console.log(massage2);
+
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("function1の出力"));
+
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("function2の出力"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
+
+const consProfile = {
+  p_name: "SYSTEX 太郎",
+  p_age: 28,
+};
+const messageProf1 =
+  "名前は${consProfile.p_name}です。年齢は${consProfile.p_age}です。";
+console.log(messageProf1);
+console.log(consProfile.p_name);
+
+const { p_name, p_age } = consProfile;
+const messageProf2 = "名前は${p_name}です。年齢は${p_age}です。";
+console.log(messageProf2);
